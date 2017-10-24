dff08c9687fe3782982f276cf3c4bf4bea4bcf13
seetaram <seetaram.taduri@gmail.com>
2017-10-24 22:52:51 +0530

    This is a demo of declaring array as an Object array

---
+++ javademos/src/com/srr/basics/ArrayTest.java
@@ -0,0 +1,16 @@
+package com.srr.basics;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+
+		// Declare an object array
+
+		Object arr[] = { 10, 20, 30, 40, 50, "seetaram", 93.5 };
+		// using foreach loop
+		for (Object i : arr)
+			System.out.println(i);
+
+	}
+
+}
