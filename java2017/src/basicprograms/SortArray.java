33e062751a5771e14497fd2554b21e94411ef9cb
seetaramtaduri <seetaramm.taduri@gmail.com>
2017-11-15 15:20:46 +0530

    sorting array elements into ascending order

---
+++ java2017/src/basicprograms/SortArray.java
@@ -0,0 +1,27 @@
+package basicprograms;
+
+public class SortArray {
+  public static void main(String[] args) {
+	int a[] = {100,200,54,21,8,9,1};
+	
+	for(int i=0;i<a.length ;i++){
+		for(int j=i+1;j<a.length;j++){
+			if(a[i]>a[j]){
+				int t = a[i];
+				a[i] = a[j];
+				a[j] = t;
+				
+			}
+		}
+	}
+	
+	System.out.print("Ascending Order:");
+    for (int i = 0; i < a.length; i++) 
+    {
+        System.out.print(a[i] + ",");
+    }
+   
+}
+	
+}
+
