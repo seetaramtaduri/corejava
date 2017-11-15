05bbe372de707b4602f388a5e6570d2ab308f884
seetaramtaduri <seetaramm.taduri@gmail.com>
2017-11-15 15:21:52 +0530

    ascending order of array

--- java2017/src/basicprograms/SortArray.java
+++ java2017/src/basicprograms/SortArray.java
@@ -5,8 +5,8 @@ public class SortArray {
 	int a[] = {100,200,54,21,8,9,1};
 	
 	for(int i=0;i<a.length ;i++){
-		for(int j=i+1;j<a.length;j++){
-			if(a[i]>a[j]){
+		for(int j=i+1;j>a.length;j++){
+			if(a[i]<a[j]){
 				int t = a[i];
 				a[i] = a[j];
 				a[j] = t;
